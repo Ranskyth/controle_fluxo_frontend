753b39478e1b21edd959507d6cd8d57d9155d916
gabriel <ga.lima2205@gmail.com>
2025-02-07 21:29:09 -0300

    feat: update page (caixa)

--- src/app/(public)/(landings)/(caixa)/page.tsx
+++ src/app/(public)/(landings)/(caixa)/page.tsx
@@ -1,6 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client";
-import { ChangeEvent, useContext, useState } from "react";
-
+import { ChangeEvent, useContext, useEffect, useState } from "react";
 import {
   Table,
   TableBody,
@@ -9,7 +9,6 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-
 import {
   Card,
   CardContent,
@@ -20,7 +19,15 @@ import {
 import { Button } from "@/components/ui/button";
 import { ContextApp } from "../../_components/context-app";
 import { Input } from "@/components/ui/input";
-
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 interface Estoque {
   id?: number;
   titulo?: string;
@@ -30,9 +37,18 @@ interface Estoque {
   createde_at?: string;
   updated_at?: string;
 }
-
 const CardVenda = (dados: Estoque) => {
-  const [Qtproduto, setQtproduto] = useState<number>(1);
+  const [quantidade, setquantidade] = useState<number>(1);
+  const [pagamento, setPagamento] = useState<string>();
+  const total = Number((quantidade * Number(dados.preco)).toFixed(2))
+
+  const handlePagamento = () => {
+    fetch(`${process.env.NEXT_PUBLIC_API}/venda/${dados.id}/produtos`,{
+      method:"POST",
+      headers:{"Content-Type":"application/json"},
+      body:JSON.stringify({total, quantidade, pagamento})
+    })
+  }
   return (
     <Card className="absolute z-10">
       <CardHeader className="flex items-center">
@@ -41,70 +57,64 @@ const CardVenda = (dados: Estoque) => {
       <CardContent>
         <CardDescription className="flex items-center gap-12">
           <div className="flex flex-col gap-2">
-          <h1>Produto : {dados.titulo}</h1>
-          <h1>Codigo Produto : {dados.id}</h1>
-          <h1>Quantidade em Estoque : {dados.quantidade}</h1>
-          <h1>Preço : R$ {dados.preco}</h1>
-          <div className="flex gap-2 items-center">
-            <Button onClick={() => setQtproduto((prev) => prev - 1)}>-</Button>
-            {Qtproduto}
-            <Button onClick={() => setQtproduto((prev) => prev + 1)}>+</Button>
+            <h1>Produto : {dados.titulo}</h1>
+            <h1>Codigo Produto : {dados.id}</h1>
+            <h1>Quantidade em Estoque : {dados.quantidade}</h1>
+            <h1>Preço : R$ {dados.preco}</h1>
+            <div className="flex gap-2 items-center">
+              <Button onClick={() => setquantidade((prev) => prev - 1)}>
+                -
+              </Button>
+              {quantidade}
+              <Button onClick={() => setquantidade((prev) => prev + 1)}>
+                +
+              </Button>
+            </div>
           </div>
-          </div>
-
           <div className="flex flex-col gap-3">
-            <h1 className="text-[1.5rem]">Forma De Pagamento</h1>
-          <Button className="bg-green-500">
-            Confirmar Venda via Pix ( R${" "}
-            {(Number(dados.preco || 0) * Qtproduto).toFixed(2)})
-          </Button>
-          <Button className="bg-green-500">
-            Confirmar Venda via Cartão de Crédito ( R${" "}
-            {(Number(dados.preco || 0) * Qtproduto).toFixed(2)})
-          </Button>
-          <Button className="bg-green-500">
-            Confirmar Venda via Cartão de Débito ( R${" "}
-            {(Number(dados.preco || 0) * Qtproduto).toFixed(2)})
-          </Button>
-          <Button className="bg-green-500">
-            Confirmar Venda via Dinheiro ( R${" "}
-            {(Number(dados.preco || 0) * Qtproduto).toFixed(2)})
-          </Button>
+            <Select onValueChange={setPagamento}>
+              <SelectTrigger className="w-[180px]">
+                <SelectValue placeholder="Tipo Pagamento" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectItem value="cartao_debito">Cartão Débito</SelectItem>
+                  <SelectItem value="cartao_credito">Cartão Crédito</SelectItem>
+                  <SelectItem value="dinheiro">Dinheiro</SelectItem>
+                  <SelectItem value="pix">Pix</SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+            <Button className="bg-green-500" onClick={handlePagamento}>Confirmar Venda (R$ {(Number(dados.preco) * quantidade).toFixed(2)})</Button>
           </div>
-
         </CardDescription>
       </CardContent>
     </Card>
   );
 };
-
 const Caixa = () => {
-  const { Logged, isLogged } = useContext(ContextApp);
+  //const { Logged, isLogged } = useContext(ContextApp);
   const [Produto, setProduto] = useState<Estoque[]>([]);
   const [HoverVenda, setHoverVenda] = useState(false);
   const [DataVendaProduto, setDataVendaProduto] = useState<Estoque>({});
-
   console.log(DataVendaProduto);
-
-  Logged();
-
-  if (!isLogged) {
-     return null;
-  }
-
+  //Logged();
+  //if (!isLogged) {
+  //   return null;
+  //}
   const handleProduto = async (e: ChangeEvent<HTMLInputElement>) => {
     console.log(e.target.value);
     const arg = await String(e.target.value);
     if (arg === "") {
       return;
     }
-    const data = await fetch(`${process.env.NEXT_PUBLIC_API}/produtos/nome/${arg}`);
+    const data = await fetch(
+      `${process.env.NEXT_PUBLIC_API}/produtos/nome/${arg}`
+    );
     const datajson = data.json();
     setProduto(await datajson);
   };
-
   console.log(HoverVenda);
-
   return (
     <>
       <div className="flex w-full flex-col items-center p-14">
@@ -124,7 +134,6 @@ const Caixa = () => {
             quantidade={DataVendaProduto.quantidade}
           />
         ) : null}
-
         <Table className="mt-9">
           <TableHeader>
             <TableRow>
